0a4ed61bcdbfcb34b9f87517d630483657aab1e8
diprofe <Federica.DIPROPERZIO@netcompany-intrasoft.com>
2023-06-14 00:28:30 +0200

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'MY Ng-app';
+  title = 'MY Ng-app'; //data binding
+  componentName = 'App component Name';
 }
