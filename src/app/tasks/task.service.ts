b9cb33bfee6736fd8f5257a82566de6c4a33d634
diprofe <Federica.DIPROPERZIO@netcompany-intrasoft.com>
2023-06-19 23:15:53 +0200

    Observables

--- src/app/tasks/task.service.ts
+++ src/app/tasks/task.service.ts
@@ -1,33 +1,39 @@
 import { Injectable } from '@angular/core';
 import { TaskItem } from './task-item.dto';
 import { NewTask } from './new-task.dto';
+import { Observable, BehaviorSubject } from 'rxjs';
 
-@Injectable({
+/*@Injectable({
   providedIn: 'root',
-})
+})*/
+
 export class TaskService {
   constructor() {}
 
-  private tasks: TaskItem[] = [
+  private tasks = new BehaviorSubject([
     new TaskItem('List1'),
     new TaskItem('List2'),
     new TaskItem('List3'),
     new TaskItem('List4'),
-  ];
+  ]);
 
-  getAllTasks(): TaskItem[] {
+  getAllTasks(): Observable<TaskItem[]> {
     return this.tasks;
   }
 
   addTask(newTask: NewTask) {
     //throw new Error('addTask Not implemented yet!');
-    this.tasks.push(new TaskItem(newTask.title));
+    //this.tasks.push(new TaskItem(newTask.title));
+    var updatedTasks = this.tasks.value.concat(new TaskItem(newTask.title));
+    this.tasks.next(updatedTasks);
   }
 
   removeTask(existingTask: TaskItem) {
-    const index = this.tasks.indexOf(existingTask);
+    var updated = this.tasks.value.filter((t) => t != existingTask);
+    this.tasks.next(updated);
+    /* const index = this.tasks.indexOf(existingTask);
     if (index !== -1) {
       this.tasks.splice(index, 1);
-    }
+    }*/
   }
 }
--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskListComponent } from './task-list.component';
 import { FormsModule } from '@angular/forms';
+import { TaskService } from './task.service';
 
 @NgModule({
   declarations: [TaskListComponent],
+  providers: [TaskService],
   imports: [CommonModule, FormsModule],
   exports: [TaskListComponent],
 })
